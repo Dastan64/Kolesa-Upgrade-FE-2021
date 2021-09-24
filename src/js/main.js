1f4160d7d241ab20904721a07c8ecaeb6c85f2c8
Dastan <dastan132000@mail.ru>
2021-09-24 22:39:02 +0300

    Сверстал десктопную версию модального окна

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,12 @@
 import '../css/style.css';
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello, Dastan Zhamekeshev!</h1>
-`;
+// document.querySelector('#app').innerHTML = `
+//   <h1>Hello, Dastan Zhamekeshev!</h1>
+// `;
+
+const modalWindowCloseIcon = document.querySelector('.modal-window__close-icon');
+const modalWindowOverlay = document.querySelector('.overlay');
+
+modalWindowCloseIcon.addEventListener('click', () => {
+    modalWindowOverlay.classList.add('overlay_hidden');
+});
