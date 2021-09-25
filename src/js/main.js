ff520a721a52c25a78fb91922a9ebfb31afc010b
Dastan <dastan132000@mail.ru>
2021-09-26 01:34:54 +0300

    Переделал кастомные радиокнопки (цвета) с изменением заливки

--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,16 @@
 const modalWindowCloseBtn = document.querySelector('.modal-window__close-btn');
 const sizesContainer = document.querySelector('.info__size-options');
 const modalWindowOverlay = document.querySelector('.overlay');
+const colorsContainer = document.querySelector('.colors');
+
+colorsContainer.addEventListener('click', (e) => {
+    if (!e.target.closest('.colors__option').classList.contains('.colors__option_active')) {
+        document.querySelectorAll('.colors__option').forEach((item) => {
+            item.classList.remove('colors__option_active');
+        });
+        e.target.closest('.colors__option').classList.add('colors__option_active');
+    }
+});
 
 modalWindowCloseBtn.addEventListener('click', () => {
     modalWindowOverlay.classList.add('overlay_hidden');
