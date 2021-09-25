ff889092659214633b1652c8e5a7defd24f2a606
Dastan <dastan132000@mail.ru>
2021-09-25 17:38:27 +0300

    Добавил переключение размеров с добавлением активного класса

--- src/js/main.js
+++ src/js/main.js
@@ -3,8 +3,16 @@
 // `;
 
 const modalWindowCloseBtn = document.querySelector('.modal-window__close-btn');
+const sizesContainer = document.querySelector('.info__size-options');
 const modalWindowOverlay = document.querySelector('.overlay');
 
 modalWindowCloseBtn.addEventListener('click', () => {
     modalWindowOverlay.classList.add('overlay_hidden');
+});
+
+sizesContainer.addEventListener('click', (e) => {
+    if (!e.target.closest('.sizes__option').classList.contains('sizes__option_active')) {
+        document.querySelectorAll('.sizes__option').forEach(item => item.classList.remove('sizes__option_active'));
+        e.target.closest('.sizes__option').classList.add('sizes__option_active');
+    }
 });
