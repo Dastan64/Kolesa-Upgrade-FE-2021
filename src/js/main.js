6a877d371b6441c3021d94d1c3b9e1603adfb004
Dastan <dastan132000@mail.ru>
2021-09-26 22:53:00 +0300

    Убрал закоментированный код в CSS и в JS-файлах

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,3 @@
-// document.querySelector('#app').innerHTML = `
-//   <h1>Hello, Dastan Zhamekeshev!</h1>
-// `;
-
 const modalWindowCloseBtn = document.querySelector('.modal-window__close-btn');
 const sizesContainer = document.querySelector('.info__size-options');
 const modalWindowOverlay = document.querySelector('.overlay');
