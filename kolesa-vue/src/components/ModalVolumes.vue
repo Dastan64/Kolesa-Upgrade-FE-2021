abe9e98890153487eaed60bb49389ce9773f4354
Dastan <dastan132000@mail.ru>
2021-10-30 11:56:58 +0300

    Стили размеров, цветов, превьюшек и объёмов перенёс + удалил статические данные (моки)

--- kolesa-vue/src/components/ModalVolumes.vue
+++ kolesa-vue/src/components/ModalVolumes.vue
@@ -14,4 +14,32 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import '../styles/variables';
+
+.volumes {
+  &__option {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    min-width: 40px;
+    min-height: 40px;
+    margin-top: 8px;
+    margin-right: 8px;
+    padding: 8px 16px;
+    border: 1px solid rgba(28, 24, 25, 0.1);
+    border-radius: 8px;
+    color: $bluePrimary;
+    cursor: pointer;
+
+    &:hover {
+      background-color: $activeBgColor;
+      color: $blackPrimary;
+    }
+
+    &--active {
+      background-color: $activeBgColor;
+      color: $blackPrimary;
+    }
+  }
+}
 </style>
